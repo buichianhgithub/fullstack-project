14a47f7774381781cb15ddf6589fc5dddb7d15fc
buichianhgithub <chianh.bui@georgebrown.ca>
2019-12-08 22:51:05 -0500

    Update master

--- src/app/components/guest-screen/guest-screen.component.ts
+++ src/app/components/guest-screen/guest-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PlayerService } from '../../services/playerService/player.service';
 
 @Component({
   selector: 'app-guest-screen',
@@ -7,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuestScreenComponent implements OnInit {
   
-  constructor() { }
-
+  displayedColumns: string[] = ['name', 'ranking', 'time', 'score', 'status', 'game', 'action'];
+  constructor(private playerService: PlayerService, public dialog: MatDialog) { }
+  players;
   ngOnInit() {
+    this.fetchPlayers();
   }
+
+
+  fetchPlayers() {
+    this.playerService.getAllPlayers().subscribe((data) => {
+      this.players = data;
+    })
+  }
+
 
   
 
