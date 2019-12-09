912fa9bf2ba217f7e1f4160a88ab1281e986b564
buichianhgithub <chianh.bui@georgebrown.ca>
2019-12-08 22:30:34 -0500

    Update master

--- src/app/components/admin-screen/updates/create-player/create-player.component.ts
+++ src/app/components/admin-screen/updates/create-player/create-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PlayerService } from './../../../../services/playerService/player.service';
 
 @Component({
   selector: 'app-create-player',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<CreatePlayerComponent>,private playerService:PlayerService) { }
 
   ngOnInit() {
   }
 
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  updatePlayer(){
+    this.closeDialog();
+    console.log("Player updated!!")
+  }
 }
